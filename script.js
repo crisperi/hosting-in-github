5f9e4226b9be63fb9a889fd046d13ab1e5ae51a9
crisperi <phoenixedian@gmail.com>
2022-05-16 11:10:35 -0700

    updated the css

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "this content added ny Javascript ";
-
-document.querySelector("body").appendChild(h2);
-
+const printBookArrow = (book) => {
+    console.log(book);
+}
+printBookArrow("its a world");
